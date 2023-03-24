8fef5215ab0e2a6ab107ae19b556b2094ff4677b
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-03-24 16:16:19 -0600

    clase clientec(listado-diferentes)

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-const nombreCliente = "Leonardo";
-const dniCliente="13804050";
-const numeroCuenta ="123434343";
+class Cliente 
+{
+  nombreCliente;
+  dniCliente;
+  numeroCuenta;
+  saldoCuenta;
+  rutCliente;
+}
+class cuentaCorriente 
+{
+   numero;
+   saldo;
+   agencia;
+
+}
+
+
+
+
+
+
+const cliente1 = new Cliente(); 
+cliente1.nombreCliente = "José";
+cliente1.dniCliente ="13232";
+cliente1.numeroCuenta = "232323";
+cliente1.saldoCuenta = 2000;
+
+const cliente2 = new Cliente();
+cliente2.nombreCliente = "Leonardo";
+cliente2.dniCliente="13804050";
+cliente2.numeroCuenta = "123434343";
+cliente2.saldoCuenta = 1000;
+
+const cliente3 = new Cliente();
+cliente3.nombreCliente = "María";
+cliente3.dniCliente = "2324343";
+cliente3.numeroCuenta = "9829382";
+cliente3.saldoCuenta = 1000;
+
+
+
+
+
+console.log(cliente1);
+console.log(cliente2);
+console.log(cliente3);
